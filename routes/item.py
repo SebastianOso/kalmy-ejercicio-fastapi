9e0f7d5c4e769bb220cb51644c01b1809616ec56
SebastianOso <sebastian.oso03@gmail.com>
2026-02-14 21:40:27 -0600

    feat: Implementando endpoint para crear item

--- routes/item.py
+++ routes/item.py
@@ -10,6 +10,32 @@ router = APIRouter(
     tags=["items"]
 )
 
+@router.post("/", response_model=ItemSchema, status_code=status.HTTP_201_CREATED)
+def crear_item(item: ItemSchema, db: Session = Depends(get_db)):
+    """
+    Crear un nuevo item
+    """
+    
+    #Validar que todos los campos esten en la request
+    if not item.name or not item.description or item.price is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="name, description y price son requeridos"
+        )
+    
+    nuevo_item = ItemModel(
+        name = item.name,
+        description= item.description,
+        price = item.price,
+        available = item.available if item is not None else True
+    )
+
+    db.add(nuevo_item)
+    db.commit()
+    db.refresh(nuevo_item)
+    return nuevo_item
+
+
 @router.get("/{item_id}", response_model=ItemSchema)
 def obtener_item(item_id: int, db: Session = Depends(get_db)):
     """
